96934b64898bc90ab5f4fd577d5d43c5edc643b8
akira-hiroshima <77510760+akira-hiroshima@users.noreply.github.com>
2021-03-16 02:48:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length <= 1) {
+	  return false }
+
+  let matchingOpeningBracket, ch, openingBrackets, closingBrackets
+  let stack = []
+	for(i=0;i<bracketsConfig.length;i++){
+	openingBrackets = bracketsConfig[i][0] //['[', '{', '(']
+	closingBrackets = bracketsConfig[i][1] //[']', '}', ')']
+  for (let j = 0; j < str.length; j++) {
+    ch = str[j]
+    if (closingBrackets.indexOf(ch) > -1) {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+        return false;
+      }
+    } else {
+      stack.push(ch)
+    }
+  }
+	}
+  return (stack.length == 0);
 }
